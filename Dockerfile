dbeb4814de688ae6b4f0c1e7d87a7739cdb413e0
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2020-06-16 07:02:22 +0200

    Improve entrypoint

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: MIT
 
 FROM librecores/ci:2020.6-rc2
-RUN pip3 install git+https://github.com/wallento/fusesoc@integration
+# TODO: This is the branch where I test integration of FuseSoC into Python
+# Needed later, but now not usable as edalized tools always print to C stdout
+# RUN pip3 install git+https://github.com/wallento/fusesoc@integration
 COPY entrypoint.py /tmp/entrypoint.py
 ENTRYPOINT [ "/tmp/entrypoint.py" ]
--- entrypoint.py
+++ entrypoint.py
@@ -3,25 +3,65 @@
 # SPDX-License-Identifier: MIT
 
 import subprocess
-import sys, os
+import sys, os, re
+import tempfile
+
+# Handle that envvars are always there, but empty
+def env_get(id):
+  v = os.getenv(id, default='')
+  if v and len(v) == 0:
+    return None
+  return v
 
 if __name__ == "__main__":
-  if "INPUT_PRE-RUN-COMMAND" in os.environ:
-    subprocess.call(os.environ.get("INPUT_PRE-RUN-COMMAND"), shell=True)
+  if env_get("INPUT_PRE-RUN-COMMAND"):
+    ret = subprocess.run(env_get("INPUT_PRE-RUN-COMMAND"), shell=True)
+    if ret.returncode != 0:
+      exit(ret.returncode)
 
-  from fusesoc.main import main
-  from fusesoc.version import version
-  print("::debug::FuseSoC version: {}".format(version))
+  _, logfile = tempfile.mkstemp(suffix=".log")
 
-  if "INPUT_ARGUMENTS" in os.environ:
-    args = os.environ.get("INPUT_ARGUMENTS").split(" ")
+  args = ["fusesoc"]
+  if env_get("INPUT_ARGUMENTS"):
+    args += ["--log-file", logfile] + env_get("INPUT_ARGUMENTS").split(" ")
   else:
-    args  = ["--cores-root", "."]
-    args += [os.environ.get("INPUT_FLOW")]
-    if "INPUT_TARGET" in os.environ:
-      args += ["--target", os.environ.get("INPUT_TARGET")]
-    if "INPUT_TOOL" in os.environ:
+    command = env_get("INPUT_COMMAND")
+    args += ["--cores-root", "."]
+    args += ["--log-file", logfile]
+    args += [command]
+    if command in ["run"]:
+      args += ["--no-export"]
+    if env_get("INPUT_TARGET"):
+      args += ["--target", env_get("INPUT_TARGET")]
+    if env_get("INPUT_TOOL"):
       args += ["--tool", os.environ.get("INPUT_TOOL")]
-    args += [os.environ.get("INPUT_CORE")]
+    if env_get("INPUT_COMMAND-ARGUMENTS"):
+      args += env_get("INPUT_COMMAND-ARGUMENTS").split(" ")
+    args += [env_get("INPUT_CORE")]
+    if env_get("INPUT_CORE-ARGUMENTS"):
+      args += env_get("INPUT_CORE-ARGUMENTS").split(" ")
 
-  exit(main(args))
+  ret = subprocess.run(" ".join(args), shell=True, capture_output=True)
+  stdout = ret.stdout
+  stderr = ret.stderr
+  rc = ret.returncode
+
+  print("=== log:")
+  with open(logfile) as f:
+    print("".join(f.readlines()))
+
+  if env_get("INPUT_COMMAND") == "run" and env_get("INPUT_TARGET") == "lint" and env_get("INPUT_TOOL") == "verilator":
+    rx = re.compile(r"%((Warning|Error)-\w+: \.\./\.\./\.\./(.*?):(\d+):(\d+): (.*?)\\n\s*:([^%]*))")
+    for m in rx.finditer(str(stderr)):
+      severity = m.group(2).lower()
+      msg = m.group(6) + "%0A"
+      msg += m.group(7).replace('%', '%25').replace('\\n', '%0A').replace('\\r', '%0D').replace("\\'", "\'")
+      print("::{} file={},line={},col={}::{}".format(severity, m.group(3), m.group(4), m.group(5), msg))
+
+  else:
+    print("=== stdout:")
+    print(str(stdout))
+    print("=== stderr:")
+    print(str(stderr))
+
+  exit(rc)
